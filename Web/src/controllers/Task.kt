85a2a3ce0ac3390645b393753d30b61d129de6c3
dan2dev <ccastro.dan@gmail.com>
2018-11-27 13:35:51 -0800

    Add task controller

--- Web/src/controllers/Task.kt
+++ Web/src/controllers/Task.kt
@@ -3,20 +3,32 @@ package ca.myclassmate.controllers
 import ca.myclassmate.models.login.LoginIndexModel
 import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.freemarker.FreeMarkerContent
 import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import io.ktor.freemarker.FreeMarkerContent
 
-fun Application.course(testing: Boolean = false) {
-    var count: Int = 0;
+
+fun Application.task(testing: Boolean = false) {
     routing {
-        get("/my-courses/") {
+        get("/my-tasks/") {
             var model = LoginIndexModel()
             call.respond(
                 FreeMarkerContent
                     (
-                    "course/course-list.ftl",
+                    "task/classroom-task.ftl",
+                    mapOf(
+                        "model" to model
+                    )
+                )
+            )
+        }
+        get("/tasks/{classroom}") {
+            var model = LoginIndexModel()
+            call.respond(
+                FreeMarkerContent
+                    (
+                    "task/classroom-task-teacher.ftl",
                     mapOf(
                         "model" to model
                     )
